c8737b5d125d97a74e8b047ca2d4608bf7e7319f
bcoggins78 <bcoggins78@gmail.com>
2019-04-14 11:35:47 -0400

    added 'movie-this'

--- liri.js
+++ liri.js
@@ -9,16 +9,17 @@ var keys = require("./keys.js");
 
 
 var searchParam = process.argv[2];
-
+var artist
+var movie
 
 
 if (searchParam == 'concert-this') {
-    var artist = process.argv[3];
+    artist = process.argv.slice(3).join("+");
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(function(response){
         
         for (var i = 0; i < 5; i++) {
         console.log("Name of the Venue: " + response.data[i].venue.name);
-        console.log("Venu Location: "+ response.data[i].venue.city + ", " + response.data[i].venue.region);
+        console.log("Venue Location: "+ response.data[i].venue.city + ", " + response.data[i].venue.region);
         var dateFormat = "YYYY/MM/DD";
         var dateTime = response.data[i].datetime
         var date = dateTime.split("T")[0];
@@ -26,6 +27,19 @@ if (searchParam == 'concert-this') {
         console.log("Date: " + convertedDate.format("MM/DD/YYYY"));
         console.log("==========================");
         }
+    },
+
+    function(error){
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
     })
 }
 
@@ -34,7 +48,40 @@ else if (searchParam == 'spotify-this-song') {
 }
 
 else if (searchParam == 'movie-this') {
-    var movie = process.argv[3];
+    movie = process.argv.slice(3).join("+");
+    if (movie == "") {
+        movie = "Mr. Nobody";
+    }
+    
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
+       
+    axios.get(queryUrl).then(
+        function(response) {
+          console.log("===================================")
+          console.log("Title: " + response.data.Title);
+          console.log("Year of Release: " + response.data.Year);
+          console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+          console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+          console.log("Country: " + response.data.Country);
+          console.log("Language: " + response.data.Language);
+          console.log("Plot: " + response.data.Plot);
+          console.log("Actors: " + response.data.Actors);
+          console.log("===================================")
+        },
+
+        function(error) {
+            if (error.response) {  
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              console.log(error.request);
+            } else {
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          }
+    );
 }
 
 else if (searchParam == 'do-what-it-says') {
